b7e6db6c46db8f6ae5c900bb3846d1f4400c354f
Maelle Vance <maellevance@gmail.com>
2016-12-13 15:09:04 -0800

    second test for response_ok

--- src/tests/test_server.py
+++ src/tests/test_server.py
@@ -5,3 +5,9 @@ def test_response_ok():
     """Test server response ok."""
     from server import response_ok
     assert response_ok().decode("utf8")[-12:] == "<CRLF><CRLF>"
+
+
+def test_number_of_crlf():
+    """Test server response contains the right amount of CRLF"""
+    from server import response_ok
+    assert response_ok().decode("utf8").count('<CRLF>') == 5
