d9710d198d48a6a1fe7094a0e2072c97459c0914
Maelle Vance <maellevance@gmail.com>
2016-12-18 13:50:11 -0800

    setting up concurrency module

---
+++ src/concurrency.py
@@ -0,0 +1,11 @@
+"""Concurrency for our server.py module."""
+
+from server import server
+
+if __name__ == '__main__':
+    from gevent.server import StreamServer
+    from gevent.monkey import patch_all
+    patch_all()
+    server = StreamServer(('127.0.0.1', 10000), server)
+    print('Starting server on port 10000')
+    server.serve_forever()
