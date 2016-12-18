3061e603eff96b71b3d0fc2b65510a09e1b5d129
Ford Fowler <fordjfowler@gmail.com>
2016-12-18 14:45:53 -0800

    Easy port changing, clean server quit

--- src/concurrency.py
+++ src/concurrency.py
@@ -1,5 +1,20 @@
 """Concurrency for our server.py module."""
+import sys
 import server
+from gevent.server import StreamServer
+from gevent.monkey import patch_all
+
+
+def start_server(port=10000):
+    """Start stream server."""
+    patch_all()
+    sserver = StreamServer(('127.0.0.1', port), connection)
+    print('Starting server on port {}'.format(port))
+    try:
+        sserver.serve_forever()
+    except KeyboardInterrupt:
+        sserver.close()
+        print('Server closed')
 
 
 def connection(socket, address):
@@ -7,10 +22,9 @@ def connection(socket, address):
     print('New connection from {}:{}'.format(address[0], address[1]))
     server.handle_connection(socket)
 
+
 if __name__ == '__main__':
-    from gevent.server import StreamServer
-    from gevent.monkey import patch_all
-    patch_all()
-    cserver = StreamServer(('127.0.0.1', 10000), connection)
-    print('Starting server on port 10000')
-    cserver.serve_forever()
+    try:
+        start_server(sys.argv[1])
+    except:
+        start_server()
